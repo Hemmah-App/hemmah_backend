de2be4a0ac872f0f616c56eda20556dbb40cc44d
abdullahsayed <abdullahsayed30@gmail.com>
2023-02-08 19:22:59 +0200

    Add standard Response for all controllers

--- src/main/java/org/help/hemah/controller/AuthController.java
+++ src/main/java/org/help/hemah/controller/AuthController.java
@@ -4,17 +4,20 @@ import jakarta.validation.Valid;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.help.hemah.helper.req_model.NewUserModel;
+import org.help.hemah.helper.res_model.Response;
 import org.help.hemah.helper.res_model.UserDataModel;
 import org.help.hemah.model.User;
 import org.help.hemah.model.embeded.BaseUserDataEntity;
 import org.help.hemah.service.token.TokenService;
 import org.help.hemah.service.user.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Slf4j
@@ -33,32 +36,66 @@ public class AuthController {
 
     @PostMapping("/signin")
     public ResponseEntity<?> signin(@RequestBody SigninForm form) {
+
+        Response.ResponseBuilder<?, ?> response = Response.builder()
+                .timeStamp(LocalDateTime.now().toString());
+
         try {
             User user = userService.signinUser(form.getEmail(), form.getPassword());
+            String token = tokenService.generateToken(user);
             log.info("Signed in: " + user);
-            return ResponseEntity.ok(Map.of("token", tokenService.generateToken(user)));
+            return ResponseEntity.ok(
+                    response
+                            .statusCode(HttpStatus.OK.value())
+                            .status(HttpStatus.OK)
+                            .message("User signed in successfully")
+                            .data(Map.of("token", token))
+                            .build());
         } catch (Exception e) {
             log.error("Error Signing User With Creeds: " + form + "reason: " + e.getMessage());
-            return ResponseEntity.badRequest().body(Map.of("Message", e.getMessage()));
+            return ResponseEntity.badRequest().body(
+                    response
+                            .statusCode(HttpStatus.BAD_REQUEST.value())
+                            .status(HttpStatus.BAD_REQUEST)
+                            .message("Error signing user with creeds: " + form)
+                            .reason(e.getMessage())
+                            .build()
+            );
         }
     }
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@Valid @RequestBody NewUserModel newUserModel) {
+        Response.ResponseBuilder<?, ?> response = Response.builder()
+                .timeStamp(LocalDateTime.now().toString());
+
         try {
             User user = userService.signNewUser(newUserModel);
+            String token = tokenService.generateToken(user);
             log.debug("new user created: " + user.getBaseUserDataEntity().getUsername());
 
-            return ResponseEntity.ok(Map.of("token", tokenService.generateToken(user)));
+            return ResponseEntity.ok(
+                    response
+                            .statusCode(HttpStatus.OK.value())
+                            .status(HttpStatus.OK)
+                            .message("User signed up successfully")
+                            .data(Map.of("token", token))
+                            .build());
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(Map.of("Message", "Error Creating User " + newUserModel + "\nreason: " + e.getMessage()));
+            return ResponseEntity.badRequest().body(
+                    response
+                            .statusCode(HttpStatus.BAD_REQUEST.value())
+                            .status(HttpStatus.BAD_REQUEST)
+                            .message("Error Creating User " + newUserModel)
+                            .reason(e.getMessage())
+                            .build());
         }
 
     }
 
     @PreAuthorize("hasRole('USER')")
     @GetMapping("/@me")
-    public UserDataModel getUserData(@AuthenticationPrincipal Jwt jwt) {
+    public ResponseEntity<?> getUserData(@AuthenticationPrincipal Jwt jwt) {
         BaseUserDataEntity baseUserData = tokenService.getUser(jwt).getUserData();
 
         UserDataModel userData = UserDataModel.builder()
@@ -71,7 +108,15 @@ public class AuthController {
                 .userType(baseUserData.getUserType())
                 .build();
 
-        return userData;
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(LocalDateTime.now().toString())
+                        .statusCode(HttpStatus.OK.value())
+                        .status(HttpStatus.OK)
+                        .message("User Data")
+                        .data(userData)
+                        .build()
+        );
     }
 
 }
---
+++ src/main/java/org/help/hemah/helper/res_model/Response.java
@@ -0,0 +1,21 @@
+package org.help.hemah.helper.res_model;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@SuperBuilder
+public class Response {
+    private String timeStamp;
+    private int statusCode;
+    private HttpStatus status;
+    private String reason;
+    private String message;
+    private String developerMessage;
+    private Object data;
+}
+
