b3276d80c7616e7d57cee3387b5975a3743f038b
abdullahsayed <abdullahsayed30@gmail.com>
2023-05-09 07:38:45 +0300

    resource loader

--- src/main/java/org/help/hemah/config/firebase/FirebaseConfig.java
+++ src/main/java/org/help/hemah/config/firebase/FirebaseConfig.java
@@ -3,18 +3,22 @@ package org.help.hemah.config.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 
 import java.io.File;
 import java.io.FileInputStream;
 
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class FirebaseConfig {
 
+    private final ResourceLoader resourceLoader;
 
     @Bean
     FirebaseApp firebaseApp(GoogleCredentials credentials) {
@@ -27,7 +31,7 @@ public class FirebaseConfig {
 
     @Bean
     GoogleCredentials googleCredentials(FirebaseProperties firebaseProperties) throws Exception {
-        File file = new File(firebaseProperties.serviceAccountFile());
+        File file = resourceLoader.getResource(firebaseProperties.serviceAccountFile()).getFile();
 
         log.info(FileUtils.readFileToString(file));
 
